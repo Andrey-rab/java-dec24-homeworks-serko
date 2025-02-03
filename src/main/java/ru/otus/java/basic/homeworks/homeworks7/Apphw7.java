5ee028e8a71e47df24b1b27d480a0319eb93c183
Andrey <andreyserko9@gmail.com>
2025-02-04 00:19:05 +0300

    first task Homework7

---
+++ src/main/java/ru/otus/java/basic/homeworks/homeworks7/Apphw7.java
@@ -0,0 +1,34 @@
+package ru.otus.java.basic.homeworks.homeworks7;
+
+public class Apphw7 {
+    public static void main(String[] args) {
+        int[][] arr = new int[5][5];
+        int[][] table = {
+                {1, -2, 3},
+                {-4, 5, 6},
+                {7, -8, 9}
+        };
+        sumOfPositiveElements(table);
+    }
+
+    /**
+     * Реализовать метод sumOfPositiveElements(..), принимающий в качестве
+     * аргумента целочисленный двумерный массив, метод должен посчитать и
+     * вернуть сумму всех элементов массива, которые больше 0;
+     */
+    public static void sumOfPositiveElements(int[][] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++ ){
+            for (int j = 0; j < array[i].length; j++ ){
+                int value = array[i][j];
+                if (value > 0){
+                    sum += value;
+                }
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println(sum);
+    }
+}
