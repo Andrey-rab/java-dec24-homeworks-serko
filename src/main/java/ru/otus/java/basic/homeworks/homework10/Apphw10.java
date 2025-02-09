49820ab2cdca248996e6a5ab125647169065eb26
Andrey <andreyserko9@gmail.com>
2025-02-09 22:56:05 +0300

    create Class User Homework10

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework10/Apphw10.java
@@ -0,0 +1,9 @@
+package ru.otus.java.basic.homeworks.homework10;
+
+public class Apphw10 {
+    public static void main(String[] args) {
+        User Alex = new User();
+        Alex.name = "Alex";
+        Alex.birthYear = 1991;
+    }
+}
---
+++ src/main/java/ru/otus/java/basic/homeworks/homework10/User.java
@@ -0,0 +1,12 @@
+package ru.otus.java.basic.homeworks.homework10;
+
+public class User {
+    /**
+     * Создайте класс Пользователь (User) с полями: фамилия, имя, отчество, год рождения, email
+     */
+    public String name;
+    public String surname;
+    public String patronymic;
+    public int birthYear;
+    public String email;
+}
