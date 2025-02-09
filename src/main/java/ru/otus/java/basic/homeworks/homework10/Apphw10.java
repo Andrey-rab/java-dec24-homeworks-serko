07d5f3223d75217f794f310e8f5aebdcd016c130
Andrey <andreyserko9@gmail.com>
2025-02-09 23:42:19 +0300

    create constructor and method printUserInfo

--- src/main/java/ru/otus/java/basic/homeworks/homework10/Apphw10.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework10/Apphw10.java
@@ -2,8 +2,7 @@ package ru.otus.java.basic.homeworks.homework10;
 
 public class Apphw10 {
     public static void main(String[] args) {
-        User Alex = new User();
-        Alex.name = "Alex";
-        Alex.birthYear = 1991;
+        User Alex = new User("Alex", "Petrov", "Ivanovich", 1991, "AlPet@mail.ru");
+        Alex.printUserInfo();
     }
 }
--- src/main/java/ru/otus/java/basic/homeworks/homework10/User.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework10/User.java
@@ -3,10 +3,30 @@ package ru.otus.java.basic.homeworks.homework10;
 public class User {
     /**
      * Создайте класс Пользователь (User) с полями: фамилия, имя, отчество, год рождения, email
+     *
+     * Реализуйте у класса конструктор, позволяющий заполнять эти поля при создании объекта
+     *
+     * В классе Пользователь реализуйте метод, выводящий в консоль информацию о пользователе в виде:
+     * ФИО: фамилия имя отчество
+     * Год рождения: год рождения
+     * e-mail: email
      */
     public String name;
     public String surname;
     public String patronymic;
     public int birthYear;
     public String email;
+
+    public User(String name, String surname, String patronymic, int birthYear, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.birthYear = birthYear;
+        this.email = email;
+    }
+    public void printUserInfo() {
+        System.out.println("ФИО: " + surname + " " + name + " " + patronymic);
+        System.out.println("Год рождения: " + birthYear);
+        System.out.println("e-mail: " + email);
+    }
 }
